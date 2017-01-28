c8a98c7715fba7c1a2b07df0adae02953ed0150f
dagronnell <3*VQCbaBWzRp>
2017-01-28 10:19:31 +0100

    it works.

--- src/main/java/dag/FrontEnd.java
+++ src/main/java/dag/FrontEnd.java
@@ -1,5 +1,6 @@
 package dag;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.sleuth.Sampler;
@@ -8,24 +9,34 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
 @EnableAspectJAutoProxy(proxyTargetClass = true)
-public class Backend {
+public class FrontEnd {
+    @Autowired
+    private RestTemplate restTemplate;
+
     public static void main(String[] args) {
-        SpringApplication.run(Backend.class,
-                "--spring.application.name=Backend",
-                "--server.port=8080");
+        SpringApplication.run(FrontEnd.class,
+                "--spring.application.name=Frontend",
+                "--server.port=8081");
     }
 
-    @RequestMapping("/api")
-    public String getApi() {
-        return "Some text";
+    @RequestMapping("/frontend")
+    public String getFrontEnd() {
+        return "From backend: " + restTemplate
+                .getForObject("http://localhost:8080/api", String.class);
     }
 
     @Bean
     public Sampler sampler() {
         return new AlwaysSampler();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
